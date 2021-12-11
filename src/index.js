dbd3890b9fb73c70cb6c592ca98348e7993f848f
Fahad Alsubaie <fahad@nrt.sa>
2021-12-11 13:18:49 +0300

    variables changes

--- src/index.js
+++ src/index.js
@@ -7,20 +7,20 @@ function initMap() {
     mapTypeId: "terrain",
   });
 
-  const flightPlanCoordinates = [
+  const lanePathCoordinates = [
     { lat: 37.772, lng: -122.214 },
     { lat: 21.291, lng: -157.821 },
     { lat: -18.142, lng: 178.431 },
     { lat: -27.467, lng: 153.027 },
   ];
-  const flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
+  const LanePath = new google.maps.Polyline({
+    path: lanePathCoordinates,
     geodesic: true,
     strokeColor: "#FF0000",
     strokeOpacity: 1.0,
     strokeWeight: 2,
   });
 
-  flightPath.setMap(map);
+  LanePath.setMap(map);
 }
 export { initMap };
